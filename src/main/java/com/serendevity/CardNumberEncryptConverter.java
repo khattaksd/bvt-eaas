f360bee2048dcbecc14f0ad4dc81e456f58079b4
khattaksd <khattaksd@gmail.com>
2021-10-13 16:19:00 -0400

    wip

--- src/main/java/com/serendevity/CardNumberEncryptConverter.java
+++ src/main/java/com/serendevity/CardNumberEncryptConverter.java
@@ -1,23 +1,34 @@
 package com.serendevity;
 
-import static java.util.Base64.getEncoder;
-
-import java.util.Base64;
-
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.core.VaultOperations;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.core.VaultTransitOperations;
+
 @Converter
 public class CardNumberEncryptConverter implements AttributeConverter<String, String> {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Override
-    public String convertToDatabaseColumn(String attribute) {
-        return getEncoder().encodeToString(attribute.getBytes());
+    public String convertToDatabaseColumn(String plaintext) {
+        return this.getTransitOps().encrypt("cardnum", plaintext);
     }
 
     @Override
-    public String convertToEntityAttribute(String dbData) {
-        return new String(Base64.getDecoder().decode(dbData));
+    public String convertToEntityAttribute(String ciphertext) {
+        return this.getTransitOps().decrypt("cardnum", ciphertext);
     }
 
+    private VaultTransitOperations getTransitOps() {
+        VaultEndpoint vaultEndpoint = new VaultEndpoint();
+        vaultEndpoint.setScheme("http");
+        VaultOperations operations = new VaultTemplate(vaultEndpoint, new TokenAuthentication("s.zfRe3rLhgbKKCoFGfH2ViiMx"));
+        return operations.opsForTransit("transit");
+    }
 }
